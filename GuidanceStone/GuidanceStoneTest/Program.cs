ca09b82c425cdbd4d4aee061ae873159fb343e71
Matt Hoffman <matt@luxeloperation.com>
2017-03-26 20:55:11 -0700

    Started stubbing in UI code.

--- GuidanceStone/GuidanceStoneTest/Program.cs
+++ GuidanceStone/GuidanceStoneTest/Program.cs
@@ -1,9 +1,6 @@
 ﻿using GuidanceStone;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using WArchiveTools;
 
 namespace GuidanceStoneTest
@@ -12,25 +9,32 @@ namespace GuidanceStoneTest
     {
         static void Main(string[] args)
         {
-            string filePath = @"E:\BreathOfTheWild\WiiUDiskImage\content\Map\MainField\A-1\A-1.11_Clustering.sblwp";
-
-            using (var fileStream = FileUtilities.LoadFile(filePath))
+            //string filePath = @"E:\BreathOfTheWild\WiiUDiskImage\content\Map\MainField\A-1\A-1.11_Clustering.sblwp";
+            string rootDirectory = @"E:\BreathOfTheWild\WiiUDiskImage\content\Map\MainField";
+            foreach(var folder in Directory.GetDirectories(rootDirectory))
             {
-                BLWP blwpFile = new BLWP();
-                blwpFile.LoadFromStream(fileStream);
-
-                foreach(var instanceHeader in blwpFile.ObjectInstances)
+                foreach(var file in Directory.GetFiles(folder, "*.sblwp"))
                 {
-                    Console.WriteLine($"Instance Name: {instanceHeader.InstanceName} Count: {instanceHeader.Instances.Count}");
-
-                    int count = 0;
-                    foreach (var instance in instanceHeader.Instances)
+                    Console.WriteLine($"Opening file {Path.GetFileName(file)}...");
+                    using (var fileStream = FileUtilities.LoadFile(file))
                     {
-                        Console.WriteLine($"{count}");
-                        Console.WriteLine($"\tPosition: {instance.Position}");
-                        Console.WriteLine($"\tRotation: {instance.Rotation}");
-                        Console.WriteLine($"\tUniScale: {instance.UniformScale}");
-                        count++;
+                        BLWP blwpFile = new BLWP();
+                        blwpFile.LoadFromStream(fileStream);
+
+                        foreach(var instanceHeader in blwpFile.ObjectInstances)
+                        {
+                            //Console.WriteLine($"Instance Name: {instanceHeader.InstanceName} Count: {instanceHeader.Instances.Count}");
+
+                            int count = 0;
+                            foreach (var instance in instanceHeader.Instances)
+                            {
+                                //Console.WriteLine($"{count}");
+                                //Console.WriteLine($"\tPosition: {instance.Position}");
+                                //Console.WriteLine($"\tRotation: {instance.Rotation}");
+                                //Console.WriteLine($"\tUniScale: {instance.UniformScale}");
+                                count++;
+                            }
+                        }
                     }
                 }
             }
---
+++ GuidanceStone/GuidanceStoneViewer/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,80 @@
+﻿using GuidanceStone;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace GuidanceStoneViewer.ViewModel
+{
+    public class MainWindowViewModel
+    {
+        public BLWP CurrentFile { get { return m_currentFile; } }
+
+        private BLWP m_currentFile;
+
+       
+
+        #region Commands
+        // File Menu
+        public ICommand NewFileCommand { get { return new RelayCommand(x => OnUserRequestNewFile()); } }
+        public ICommand OpenFileCommand { get { return new RelayCommand(x => OnUserRequestOpenFile()); } }
+        public ICommand SaveFileCommand { get { return new RelayCommand(x => OnUserRequestSaveFile(), x => CurrentFile != null); } }
+        public ICommand SaveFileAsCommand { get { return new RelayCommand(x => OnUserRequestSaveFileAs(), x => CurrentFile != null); } }
+        public ICommand CloseFileCommand { get { return new RelayCommand(x => OnUserRequestCloseFile(), x => CurrentFile != null); } }
+        public ICommand ExitApplicationCommand { get { return new RelayCommand(x => OnUserRequestExitApplication()); } }
+
+        // Help Menu
+        public ICommand OpenReportABugCommand { get { return new RelayCommand(x => OnUserRequestReportBug()); } }
+        public ICommand OpenWikiCommand { get { return new RelayCommand(x => OnUserRequestOpenWiki()); } }
+        public ICommand OpenAboutDialogCommand { get { return new RelayCommand(x => OnUserRequestOpenAboutDialog()); } }
+
+        private void OnUserRequestCloseFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnUserRequestSaveFileAs()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnUserRequestSaveFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnUserRequestOpenFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnUserRequestNewFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnUserRequestExitApplication()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        private void OnUserRequestReportBug()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnUserRequestOpenWiki()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnUserRequestOpenAboutDialog()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
